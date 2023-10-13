585b33e1ad0fc56bb04cad14903763f709c119da
Adolf <adolfnyerere@gmail.com>
2023-10-13 12:23:16 +0300

    Added server-side

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let newId = document.getElementById("text");
+
+})
+
+const loginButton = document.getElementById("loginButton");
+function login() {
+    alert("Login button clicked!");
+}
+
+loginButton.addEventListener("click", login);
